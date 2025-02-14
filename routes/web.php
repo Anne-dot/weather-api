0c0cc4b8292d872f1d43690af5e31ce18d047d68
Anne-Dot <ruusmann@gmail.com>
2025-02-14 14:15:19 +0200

    cleaned up project files

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
 Route::post('/', [WeatherController::class, 'search'])->name('weather.search');
-
-
-
-
-
-// Route::get('/weather/{city}', [WeatherController::class, 'getWeather'])->name('weather.city');
